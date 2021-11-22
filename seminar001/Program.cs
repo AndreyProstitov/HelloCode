e479cdef2081ac181bfe593cae1edd9c2d9c8d3d
Andrey <asics1313@yandex.ru>
2021-11-22 21:05:20 +0300

    1numb chalange

--- seminar001/Program.cs
+++ seminar001/Program.cs
@@ -1,2 +1,13 @@
 ﻿int numberA = 5;
-Console.WriteLine(numberA*numberA);
+Console.WriteLine(numberA);
+int numberB = 6;
+Console.WriteLine(numberB);
+int result = numberA*numberA ;
+if (numberB == result)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
